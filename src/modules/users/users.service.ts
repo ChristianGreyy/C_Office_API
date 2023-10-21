d08f38d4ab6f8cc84e789657ca363b45fa0044d7
ChristianGreyy <truongthanhhung2k2@gmail.com>
2023-10-21 23:47:12 +0700

    feat(project base): #3 set up prisma

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class UsersService {
+  constructor(private readonly prisma: PrismaService) {}
+
   async getUsers(): Promise<any> {
-    return 'users';
+    const users = await this.prisma.user.findMany();
+    return users;
   }
 }
